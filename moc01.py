d6ae34eec29d76c4dcf583dd3a9e0ed72d70cd67
tomoyk <tmyk.kym@gmail.com>
2017-07-05 00:03:00 +0900

    diff使えるようになった.結果が合わないから考察せよ.

--- moc01.py
+++ moc01.py
@@ -1,22 +1,28 @@
-#!/usr/bin/env python
+]#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from sympy import *
 
-x1 = 5.0
+# シンボルの設定
+x = symbols('x')
 
-def F(x):
-  return x*x-2
+# 変数の初期化
+x1 = 5
+counter = 0;
+formula = diff(x**2-2, x)
 
-def Fd(x):
-  # return diff(x*x-2, x)
-  return 2*x
+def F(ar_X):
+  return ar_X**2-2
+
+def Fd(ar_x):
+  return formula.subs(x, ar_x)
 
 while True:
   x2 = x1 - F(x1) / Fd(x1)
 
-  if abs(x2 - x1) < 0.0001:
+  if abs(x2 - x1) < 0.000000001:
     break
 
   x1 = x2
 
-  print(x1)
+  counter+=1
+  print(counter,"-",x1)
