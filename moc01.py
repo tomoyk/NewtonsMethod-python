004414a57f3d27b16a1d97c6d6e76789cd9d41d9
tomoyk <tmyk.kym@gmail.com>
2017-07-06 00:21:20 +0900

    finished

--- moc01.py
+++ moc01.py
@@ -41,4 +41,8 @@ def sqrt(num):
 
   return x1
 
-print("√3 =",sqrt(3))
+# メイン関数
+if __name__ == '__main__':
+  nums = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97]
+  for num in nums:
+    print("√",num,"=",sqrt(num))
